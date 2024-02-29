5ec5bb9b5661c7a18fe0de1daf6e6d1047fb06c1
intelliDean <intelliDean@gmail.com>
2024-02-29 02:05:58 +0100

    updated functions

--- contracts/Err.sol
+++ contracts/Err.sol
@@ -11,4 +11,11 @@ contract Err {
     error YOU_ARE_NOT_LOGGED_IN__LOG_IN_TO_CONTINUE();
     error COULD_NOT_CREATE_NFT();
     error YOU_ARE_NOT_A_MEMBER_OF_THIS_GROUP();
+    error CONTENT_TOO_LONG();
+    error NAME_TOO_LONG();
+    error NAME_OR_USERNAME_CANNOT_EMPTY();
+    error URI_CANNOT_BE_EMPTY();
+    error GROUP_NAME_CANNOT_BE_EMPTY();
+    error CONTENT_CANNOT_BE_EMPTY();
+    error NAME_CANNOT_BE_EMPTY();
 }
--- contracts/NFTSocialMedia.sol
+++ contracts/NFTSocialMedia.sol
@@ -1,8 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-//import "contracts/socialMedia/INFTFactory.sol";
-//import "contracts/socialMedia/NFTFactory.sol";
 import "./Err.sol";
 import "./NFTFactory.sol";
 
@@ -10,29 +8,38 @@ import "./NFTFactory.sol";
 contract NFTSocialMedia {
 
     address private owner;
+    uint8 private constant CONTENT_MAX_LENGTH = 120;
+    uint8 private constant NAME_MAX_LENGTH = 20;
+
     NFTFactory private nftFactory;
 
-    Post[] feeds;
+    Post[] private feeds;
     mapping(address => Account) private accounts;
     mapping(address => bool) private loggedIn;
 
-    mapping(address => mapping(address => bool)) private followers;
-    mapping(address => User) private users;
-    mapping(address => address) private userNFTContracts;
 
+    mapping(address => User) private users;
+    mapping(bytes32 => Group) private groups;
     mapping(bytes32 => Post) private posts;
     mapping(bytes32 => Comment) private comments;
+    mapping(address => address) private userNFTContracts;
+    mapping(address => mapping(address => bool)) private followers;
     mapping(address => mapping(bytes32 => bool)) private postLikes;
     mapping(address => mapping(bytes32 => bool)) private commentLikes;
-    mapping(bytes32 => Group) private groups;
+
+
     mapping(address => mapping(bytes32 => bool)) private groupMembers;
     mapping(bytes32 => mapping(bytes32 => Post)) private postsInGroup;
     mapping(address => mapping(bytes32 => mapping(bytes32 => bool))) private likePostInGroup;
 
-
     event Registered(address userId, address nftFactoryAddress, bool success);
     event LoggedIn(address userId, bool success);
     event CreatePost(address userId, bytes32 postId, bool success);
+    event CommentOnPost(address commenter, bytes32 _postId, bytes32 commentId);
+    event CreateGroup(address groupOwner, bytes32 _groupId, bool success);
+    event PostOnGroup(address ownerOfPost, bytes32 _groupId, bytes32 _postId);
+    event CommentOnGroupPost(address commenter, bytes32 groupId, bytes32 postId, bytes32 commentId);
+
 
     enum ROLE {
         ADMIN, USER
@@ -46,7 +53,8 @@ contract NFTSocialMedia {
     struct Group {
         bytes32 groupId;
         address groupOwner;
-        address[] member;
+        string groupName;
+        address[] members;
         uint256 createAt;
         Post[] groupPosts;
     }
@@ -77,9 +85,9 @@ contract NFTSocialMedia {
         uint256 likes;
     }
 
-    constructor(address _nftContract) {
+    constructor(address _nftFactoryContract) {
         owner = msg.sender;
-        nftFactory = NFTFactory(_nftContract);
+        nftFactory = NFTFactory(_nftFactoryContract);
     }
 
     function checkAddressZero() private view {
@@ -94,6 +102,16 @@ contract NFTSocialMedia {
         if (users[msg.sender].role != ROLE.USER) revert Err.ONLY_USER();
     }
 
+    function contentLength(string memory _content) private pure {
+        if (bytes(_content).length < 1) revert Err.CONTENT_CANNOT_BE_EMPTY();
+        if (bytes(_content).length > CONTENT_MAX_LENGTH) revert Err.CONTENT_TOO_LONG();
+    }
+
+    function nameLength(string memory _name) private pure {
+        if (bytes(_name).length < 1) revert Err.NAME_CANNOT_BE_EMPTY();
+        if (bytes(_name).length > NAME_MAX_LENGTH) revert Err.NAME_TOO_LONG();
+    }
+
     function isLoggedIn() private view {
         if (!loggedIn[msg.sender]) revert Err.YOU_ARE_NOT_LOGGED_IN__LOG_IN_TO_CONTINUE();
     }
@@ -102,14 +120,13 @@ contract NFTSocialMedia {
         if (!groupMembers[msg.sender][_groupId]) revert Err.YOU_ARE_NOT_A_MEMBER_OF_THIS_GROUP();
     }
 
-    function registerUser(
-        string calldata _fullName,
-        string calldata _username,
-        string memory _nftName,
-        string memory _nftSymbol
-    ) external {
+    function registerUser(string calldata _fullName, string calldata _username, string memory _nftName, string memory _nftSymbol) external {
         checkAddressZero();
         if (users[msg.sender].userId != address(0)) revert Err.ALREADY_REGISTERED__LOGIN();
+        nameLength(_fullName);
+        nameLength(_username);
+        nameLength(_nftName);
+        nameLength(_nftSymbol);
 
         User storage _user = users[msg.sender];
         _user.userId = msg.sender;
@@ -140,7 +157,7 @@ contract NFTSocialMedia {
 
     function logout() external returns (bool) {
         checkAddressZero();
-        onlyUser();
+        isLoggedIn();
 
         loggedIn[msg.sender] = false;
         emit LoggedIn(msg.sender, true);
@@ -151,10 +168,13 @@ contract NFTSocialMedia {
     function createPost(string memory _content, string memory _uri) external {
         checkAddressZero();
         isLoggedIn();
+        contentLength(_content);
+        if (bytes(_uri).length < 1) revert Err.URI_CANNOT_BE_EMPTY();
 
         bytes32 _postId = keccak256(abi.encodePacked(msg.sender, _content, block.timestamp));
 
-        if (!nftFactory.mintNFT(msg.sender, _uri)) revert Err.COULD_NOT_CREATE_NFT();
+        //will uncomment this when i switch to testnet
+        // if (!nftFactory.mintNFT(msg.sender, _uri)) revert COULD_NOT_CREATE_NFT();
 
         Post storage _post = posts[_postId];
         _post.postId = _postId;
@@ -170,13 +190,22 @@ contract NFTSocialMedia {
     }
 
     function getPost(bytes32 _postId) external view returns (Post memory) {
-
         return posts[_postId];
     }
 
-    function commentOnPost(bytes32 _postId, string memory _content) external {
-        onlyUser();
+    function likeAndUnlikePost(bytes32 _postId) external {
         isLoggedIn();
+
+        Post storage _post = posts[_postId];
+
+        _post.likes = postLikes[msg.sender][_postId] ? (_post.likes - 1) : (_post.likes + 1);
+        postLikes[msg.sender][_postId] = !postLikes[msg.sender][_postId];
+    }
+
+    function commentOnPost(bytes32 _postId, string memory _content) external {
+        checkAddressZero();
+        isLoggedIn();
+        contentLength(_content);
 
         bytes32 commentId = keccak256(abi.encodePacked(msg.sender, _postId, _content));
 
@@ -187,71 +216,74 @@ contract NFTSocialMedia {
         _comment.commentTime = block.timestamp;
 
         posts[_postId].comments.push(_comment);
+        // comments[commentId] = _comment;
+
+        emit CommentOnPost(msg.sender, _postId, commentId);
     }
 
-    function likeAndUnlikePost(bytes32 _postId) external {
-        onlyUser();
-        isLoggedIn();
-
-        Post storage _post = posts[_postId];
-
-        if (postLikes[msg.sender][_postId]) {
-            _post.likes = _post.likes - 1;
-            postLikes[msg.sender][_postId] = false;
-        } else {
-            _post.likes = _post.likes + 1;
-            postLikes[msg.sender][_postId] = true;
-        }
+    function getPostComment(bytes32 _commentId) external view returns (Comment memory) {
+        return comments[_commentId];
     }
 
-    function likeAndUnlikeComment(bytes32 _commentId) external {
-        onlyUser();
+    function likeAndUnlikeComment(bytes32 _postId, bytes32 _commentId) external {
         isLoggedIn();
 
         Comment storage _comment = comments[_commentId];
-        if (commentLikes[msg.sender][_commentId]) {
-            _comment.likes = _comment.likes - 1;
-            commentLikes[msg.sender][_commentId] = false;
-        } else {
-            _comment.likes = _comment.likes + 1;
-            commentLikes[msg.sender][_commentId] = true;
-        }
+        _comment.likes = commentLikes[msg.sender][_commentId] ? (_comment.likes - 1) : (_comment.likes + 1);
+        commentLikes[msg.sender][_commentId] = !commentLikes[msg.sender][_commentId];
+
+        posts[_postId].comments.push(_comment);
     }
 
-    function userCreateGroup() external {
-        onlyUser();
+    function userCreateGroup(string memory _groupName) external {
+        checkAddressZero();
         isLoggedIn();
-        uint256 _time = block.timestamp;
+        nameLength(_groupName);
+        if (bytes(_groupName).length < 1) revert Err.GROUP_NAME_CANNOT_BE_EMPTY();
 
-        bytes32 _groupId = keccak256(abi.encodePacked(msg.sender, _time));
+        uint256 _time = block.timestamp;
+        bytes32 _groupId = keccak256(abi.encodePacked(msg.sender, _groupName, _time));
         Group storage _group = groups[_groupId];
         _group.groupId = _groupId;
+        _group.groupName = _groupName;
         _group.groupOwner = msg.sender;
         _group.createAt = _time;
+
+        // creator is added to the group
+        _group.members.push(msg.sender);
+        groupMembers[msg.sender][_groupId] = true;
+
+        emit CreateGroup(msg.sender, _groupId, true);
+    }
+
+    function getGroup(bytes32 _groupId) external view returns (Group memory) {
+        return groups[_groupId];
     }
 
     function usersJoinGroup(bytes32 _groupId) external {
-        onlyUser();
+        checkAddressZero();
         isLoggedIn();
 
         Group storage _group = groups[_groupId];
 
         groupMembers[msg.sender][_groupId] = true;
-        _group.member.push(msg.sender);
+        _group.members.push(msg.sender);
     }
 
     function membersPostOnGroup(bytes32 _groupId, string memory _content, string memory _uri) external {
-        onlyUser();
         isLoggedIn();
         onlyGroupMember(_groupId);
+        contentLength(_content);
 
         uint256 _time = block.timestamp;
 
         bytes32 _postId = keccak256(abi.encodePacked(msg.sender, _content, _time));
 
-        if (!nftFactory.mintNFT(msg.sender, _uri)) revert Err.COULD_NOT_CREATE_NFT();
+        //will uncomment this in testnet
+        //if (!nftFactory.mintNFT(msg.sender, _uri)) revert COULD_NOT_CREATE_NFT();
 
-        Post storage _post = postsInGroup[_groupId][_postId];
+        //    Post storage _post =  postsInGroup[_groupId][_postId];
+        Post storage _post = posts[_postId];
         _post.postId = _postId;
         _post.content = _content;
         _post.nftURI = _uri;
@@ -260,22 +292,47 @@ contract NFTSocialMedia {
         Group storage _group = groups[_groupId];
         _group.groupPosts.push(_post);
 
+        emit PostOnGroup(msg.sender, _groupId, _postId);
     }
 
-
     function memberLikesAndUnlikePostOnGroup(bytes32 _groupId, bytes32 _postId) external {
-        onlyUser();
         isLoggedIn();
         onlyGroupMember(_groupId);
 
-        Post storage _post = postsInGroup[_groupId][_postId];
+        Post storage _post = posts[_postId];
+        _post.likes = postLikes[msg.sender][_postId] ? (_post.likes - 1) : (_post.likes + 1);
+        postLikes[msg.sender][_postId] = !postLikes[msg.sender][_postId];
+    }
 
-        if (likePostInGroup[msg.sender][_groupId][_postId]) {
-            _post.likes = _post.likes - 1;
-            likePostInGroup[msg.sender][_groupId][_postId] = false;
-        } else {
-            _post.likes = _post.likes + 1;
-            likePostInGroup[msg.sender][_groupId][_postId] = true;
-        }
+    function membersCommentOnPostOnGroup(bytes32 _groupId, bytes32 _postId, string memory _content) external {
+        checkAddressZero();
+        isLoggedIn();
+        onlyGroupMember(_groupId);
+        contentLength(_content);
+
+        bytes32 commentId = keccak256(abi.encodePacked(msg.sender, _postId, _content));
+
+        Comment storage _comment = comments[commentId];
+        _comment.commenter = msg.sender;
+        _comment.commentId = commentId;
+        _comment.content = _content;
+        _comment.commentTime = block.timestamp;
+
+        posts[_postId].comments.push(_comment);
+
+        emit CommentOnGroupPost(msg.sender, _groupId, _postId, commentId);
+    }
+
+    function membersLikeCommentOnPostsOnGroup(bytes32 _groupId, bytes32 _postId, bytes32 _commentId) external {
+        checkAddressZero();
+        isLoggedIn();
+        onlyGroupMember(_groupId);
+
+        Post storage _post = posts[_postId];
+        _post.likes = commentLikes[msg.sender][_commentId] ? (_post.likes - 1) : (_post.likes + 1);
+        commentLikes[msg.sender][_commentId] = !commentLikes[msg.sender][_commentId];
     }
 }
+
+
+
